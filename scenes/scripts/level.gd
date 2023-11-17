ad7c46aeaeb2d5c7276054fdd53cf7ea7fa5c23b
VargrSkaoi <vargrskaoi@gmail.com>
2023-11-16 18:06:10 -0600

    learned about scripts interacting with other scripts

--- scenes/scripts/level.gd
+++ scenes/scripts/level.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
 func _ready():
-	print("level node is ready")
+#	print("level node is ready")
 #	get_node("Logo").rotation_degrees = 90
 	$Logo.rotation_degrees = 90
 	
 func _process(delta):
 	$Logo.rotation_degrees += 1
+	
+#	if($Logo.rotation_degrees > 180):
+#		$Logo.rotation_degrees = 0
+		
+	if($Logo.pos.x > 1000):
+		print("position exceeded")
+		$Logo.pos = Vector2.ZERO
