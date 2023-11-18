f73f5d0244ace26af5bdf39da5dd954d98fd6261
VargrSkaoi <vargrskaoi@gmail.com>
2023-11-18 10:47:40 -0600

    added actions to primary and secondary actions

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -2,4 +2,10 @@ extends Node2D
 
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 200 * delta
+	position += direction * 500 * delta
+	
+	if(Input.is_action_pressed("primary_action")):
+		print("shoot lazer")
+		
+	if(Input.is_action_pressed("secondary_action")):
+		print("shoot grenade")
