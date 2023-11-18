e4f7dfc8f4a6149086fde18cbfb793c738550693
VargrSkaoi <vargrskaoi@gmail.com>
2023-11-18 10:54:51 -0600

    learned about unique nodes

--- scenes/scripts/level.gd
+++ scenes/scripts/level.gd
@@ -11,13 +11,17 @@ func _ready():
 #		print(i)
 #
 #	print(test_array[0])
+	print($Sprite2D/Sprite2D2/Sprite2D3/Input)
+	print(%Input)
 	
 func _process(delta):
 	$Logo.rotation_degrees += 100 * delta
 	
 #	if($Logo.rotation_degrees > 180):
 #		$Logo.rotation_degrees = 0
-		
+	
 	if($Logo.pos.x > 1000):
 		print("position exceeded")
-		$Logo.pos = Vector2.ZERO
+		$Logo.pos.x = 0
+	
+#	print(Input.is_action_pressed("left"))
