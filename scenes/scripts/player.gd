59d972c6a4a138c86040b4739e81e06878b8baf1
VargrSkaoi <vargrskaoi@gmail.com>
2023-11-18 11:19:26 -0600

    removed logo scene and logo script

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -1,8 +1,9 @@
-extends Node2D
+extends CharacterBody2D
 
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 500 * delta
+	velocity = direction * 500
+	move_and_slide()
 	
 	if(Input.is_action_pressed("primary_action")):
 		print("shoot lazer")
